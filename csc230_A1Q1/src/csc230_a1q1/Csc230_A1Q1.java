bd1eeb6260846fb276d8e8d6c60fe5e97ab9f0a8
dchardin <dch84121@gmail.com>
2017-02-12 16:15:14 -0500

    updating csc230_A1Q1

--- csc230_A1Q1/src/csc230_a1q1/Csc230_A1Q1.java
+++ csc230_A1Q1/src/csc230_a1q1/Csc230_A1Q1.java
@@ -4,6 +4,8 @@
 */
 package csc230_a1q1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author donnie
@@ -14,7 +16,18 @@ public class Csc230_A1Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner input = new Scanner (System.in);
+        
+        String name = "null";
+        
+        System.out.println("Please enter your name: ");
+        
+        name = input.next();
+        
+        System.out.print(name);
+        
+        
     }
     
 }
