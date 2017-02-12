a2825daf446ff73fe9366cdc78491c3d3ff39d0e
dchardin <dch84121@gmail.com>
2017-02-12 15:35:42 -0500

    adding project csc230_A1Q1 to the repo.

---
+++ csc230_A1Q1/src/csc230_a1q1/Csc230_A1Q1.java
@@ -0,0 +1,20 @@
+/* 
+ * This program will take an arbitrary string as a parameter. It will then 
+ * print that string out on the console screen.
+*/
+package csc230_a1q1;
+
+/**
+ *
+ * @author donnie
+ */
+public class Csc230_A1Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
