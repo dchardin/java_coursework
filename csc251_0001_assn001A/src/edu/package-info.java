c18032270cd35a82b8778f20bb6246d657c2a875
dchardin <dch84121@gmail.com>
2018-08-22 16:33:32 -0400

    initial commit of csc251_0001_assn001A

---
+++ csc251_0001_assn001A/src/edu/package-info.java
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Course:   CSC 151 Spring 2014
+//  Section:  (your section number)
+// 
+//  Project:  csc251_0001_assn001A
+//  File:     package-info.java
+//  
+//  Name:     (your first and last name)
+//  Email:    (your Wake Tech Email Address)
+////////////////////////////////////////////////////////////////////////////////
+/**
+ * (Insert a comment that briefly describes the purpose of this class definition.)
+ *
+ * <p/> Bugs: (List any known issues or unimplemented features here)
+ * 
+ * @author (Insert your first and last name)
+ *
+ */
+package edu;
---
+++ csc251_0001_assn001A/src/edu/waketech/csc251/hr/person/Employee.java
@@ -0,0 +1,57 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Course:   CSC 151 Spring 2014
+//  Section:  (your section number)
+// 
+//  Project:  csc251_0001_assn001A
+//  File:     Employee.java
+//  
+//  Name:     (your first and last name)
+//  Email:    (your Wake Tech Email Address)
+////////////////////////////////////////////////////////////////////////////////
+package edu.waketech.csc251.hr.person;
+
+/**
+ * 
+ * Write a class Employee with a name and salary.  
+ * Have (at least) getters for name and salary.  
+ * Whether you have setters or some other mechanism 
+ * to set name and salary is up to you.  
+ * Employee should be in package edu.waketech.csc251.hr.person.
+ * 
+ * <p/> Bugs: (List any known issues or unimplemented features here)
+ * 
+ * @author (Insert your first and last name)
+ *
+ */
+public class Employee
+{
+	private String name;
+	private double salary;
+	
+	public Employee(String name, double salary) 
+	{
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public double getSalary()
+	{
+		return salary;
+	}
+	
+	public void setSalary(double salary)
+	{
+		this.salary = salary;
+	}
+	
+}
